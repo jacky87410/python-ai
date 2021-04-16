092c8cd5708abe0b67ce4de03be7faadc166be3a
jacky87410 <jacky87410@gmail.com>
2021-04-16 16:07:53 +0800

    D21

--- D21-1.py
+++ D21-1.py
@@ -7,8 +7,8 @@ print(df.head())
 #題目 : 將資料夾中 boston.csv 讀進來，並用圖表分析欄位。
 #1.畫出箱型圖，並判斷哪個欄位的中位數在 300~400 之間？
 #key B欄位的中位數介於300~400
-# plt.boxplot(df)
-# plt.show()
+plt.boxplot(df)
+plt.show()
 print(df.columns[10],df.columns[12])
 #2.畫出散佈圖 x='NOX', y='DIS' ，並說明這兩欄位有什麼關係？
 #兩個欄位成反比，即一邊大另一邊就會小
