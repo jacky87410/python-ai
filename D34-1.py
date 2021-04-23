f4bac7ac70436815aea12611f1357a6d493f71e5
jacky87410 <jacky87410@gmail.com>
2021-04-23 14:23:42 +0800

    D34-1

---
+++ D34-1.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import math 
+import statistics 
+from scipy import stats 
+#如果拒絕域改變時，α 和 β 會怎樣變化。
+#1.   假設 x̄ > 2，拒絕 H0，α 會變大／變小？β 會變大／變小？
+α變小、β變大
+#2.   假設 x̄ > 0.2，拒絕 Ho，會變大／變小？β 會變大/變小？
+α變大、β變小
+#3.   α 和 β 有關聯性？
+α、β呈現負相關
