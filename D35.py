7aa4dd2bf3338dab3c95becf6b33409200ddc541
jacky87410 <jacky87410@gmail.com>
2021-04-23 15:28:49 +0800

    D35

--- D35.py
+++ D35.py
@@ -1,14 +1,21 @@
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as plt 
+import math  
 import seaborn as sns 
-import math 
-import statistics 
-from scipy import stats 
-#如果拒絕域改變時，α 和 β 會怎樣變化。
-#1.   假設 x̄ > 2，拒絕 H0，α 會變大／變小？β 會變大／變小？
-α變小、β變大
-#2.   假設 x̄ > 0.2，拒絕 Ho，會變大／變小？β 會變大/變小？
-α變大、β變小
-#3.   α 和 β 有關聯性？
-α、β呈現負相關
+import matplotlib.pyplot as plt 
+import statistics
+from scipy import stats
+import statsmodels.stats.proportion
+#作業 1：用 A/B test 幫工廠做決定吧
+#某工廠想知道兩條不同的生產線的產品不良率是否有所不同，由兩條生產線中各抽取 300 個樣本，第一組有 75 個不良品，第二組有 30 個不良品，我們可以宣稱生產線所生產出的產品不良率不相同？(以0.05 為顯著水準)？
+#(提示：透過課程投影片的步驟，需思考 H0、H1 的寫法範例不同唷。)
+#HO:生產線的產品不良率相同
+#H1:生產線的產品不良率不同
+A = [75,30]
+B = [300,300]
+r = statsmodels.stats.proportion.proportions_ztest(A,B,alternative = 'two-sided')
+print(r[1])
+#P-value小於0.05拒絕虛無假設，所以兩生產線產品不良率不同
+#作業 2：你的工作，有需要 A/B test 幫你做決定？
+#不需要
+#可以在論壇中，寫出你的問題，嘗試用今天課程教的方法，透過 5個步驟的拆解，計算出結果，透過統計輔助你做決策。
